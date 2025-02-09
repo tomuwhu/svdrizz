9cafdbdb15264eeae7f5fbd22aa609abf15a285a
Dr. Németh Tamás <tomuwhu@gmail.com>
2025-02-09 20:42:48 +0100

    .

--- src/lib/server/db/schema.js
+++ src/lib/server/db/schema.js
@@ -1,7 +1,7 @@
 import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core'
 
 export const animals = sqliteTable('animals', {
-	id: integer('id').primaryKey(),
-	name: text('name'),
-	age: integer('age')
+	id: integer().primaryKey(),
+	name: text(),
+	age: integer()
 })
